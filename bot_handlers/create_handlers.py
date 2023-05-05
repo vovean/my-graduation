859ef587b75c1967dca2528f665e3e8e5210ba74
Vislovich Vladimir <vladimir.vislovich@sbermegamarket.ru>
2023-05-05 13:27:49 +0500

    help command

--- bot_handlers/create_handlers.py
+++ bot_handlers/create_handlers.py
@@ -5,6 +5,7 @@ from telegram.ext import BaseHandler
 import db as dbm
 from .file_download import FileDownloadHandler
 from .file_upload import FileUploadHandler
+from .help import HelpHandler
 from .login import LoginHandler
 from .logout import LogoutHandler
 from .rendless import RendlessHandler
@@ -21,6 +22,7 @@ def create_handlers(db: dbm.Database, data_path: pathlib.Path, root_path: pathli
         *RendlessHandler(db, data_path).get_handler(),
         FileUploadHandler(db, root_path).get_handler(),
         FileDownloadHandler(db).get_handler(),
+        HelpHandler().get_handler(),
     ]
 
     return handlers
